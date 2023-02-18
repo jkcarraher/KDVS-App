206374d3d92e90b9acd69f46aa6fab661477027d
John Carraher <jkcarraher@gmail.com>
2023-02-17 18:36:55 -0800

    Initial Commit

---
+++ KDVS App/KDVS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDVS_AppApp.swift
+//  KDVS App
+//
+//  Created by John Carraher on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct KDVS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
