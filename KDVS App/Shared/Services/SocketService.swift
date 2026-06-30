a3636bb9535f747f2e0bc34190e259c747089f81
John Carraher <jkcarraher@gmail.com>
2026-06-30 11:58:09 -0700

    Task #25: Removing complexity

--- KDVS App/Shared/Services/SocketService.swift
+++ KDVS App/Shared/Services/SocketService.swift
@@ -12,19 +12,9 @@ final class SocketService {
 
     static let shared = SocketService()
 
-    private let manager = SocketManager(
-        socketURL: Socket.server!,
-        config: [
-            .log(true),
-            .compress
-        ]
-    )
+    private let manager = SocketManager( socketURL: Socket.server! )
 
     private lazy var socket = manager.defaultSocket
-
-    private init() {
-        setupHandlers()
-    }
 
     func connect() {
         socket.connect()
@@ -40,27 +30,5 @@ final class SocketService {
 
     func stopListening() {
         socket.emit("stopListening")
-    }
-
-    private func setupHandlers() {
-
-        socket.on(clientEvent: .connect) { data, ack in
-            print("Socket connected")
-        }
-
-        socket.on(clientEvent: .disconnect) { data, ack in
-            print("Socket disconnected")
-        }
-
-        socket.on("listenerCount") { data, ack in
-            guard
-                let payload = data.first as? [String: Any],
-                let count = payload["count"] as? Int
-            else {
-                return
-            }
-
-            print("Current listeners: \(count)")
-        }
     }
 }
